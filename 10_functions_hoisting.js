0beaf1a506abdee7507b1166b12215ce20008e27
Salomon Laleye <salomonlaleye@gmail.com>
2020-05-17 18:44:12 +0200

    Add functions hoisting exercise

--- 10_functions_hoisting.js
+++ 10_functions_hoisting.js
@@ -1,61 +1,17 @@
-function getStudentFromId(studentId) {
-    return studentRecords.find(matchId);
-    // Reads cleaner
-    function matchId(record){
-		return (record.id == studentId);
-	}
-}
+// Because of function hoisting, we move all functions declarations at the bottom
 
-function printRecords(recordIds) {
-	var records = recordIds.map(getStudentFromId);
-
-	records.sort(function sortByNameAsc(record1,record2){
-		if (record1.name < record2.name) return -1;
-		else if (record1.name > record2.name) return 1;
-		else return 0;
-	});
-
-	records.forEach(function printRecord(record){
-		console.log(`${record.name} (${record.id}): ${record.paid ? "Paid" : "Not Paid"}`);
-	});
-}
-
-function paidStudentsToEnroll() {
-	var recordsToEnroll = studentRecords.filter(function needToEnroll(record){
-		return (record.paid && !currentEnrollment.includes(record.id));
-	});
-
-	var idsToEnroll = recordsToEnroll.map(function getStudentId(record){
-		return record.id;
-	});
-
-	return [ ...currentEnrollment, ...idsToEnroll ];
-}
-
-function remindUnpaid(recordIds) {
-	var unpaidIds = recordIds.filter(function notYetPaid(studentId){
-		var record = getStudentFromId(studentId);
-		return !record.paid;
-	});
-
-	printRecords(unpaidIds);
-}
-
-
-// ********************************
-
-var currentEnrollment = [ 410, 105, 664, 375 ];
+var currentEnrollment = [410, 105, 664, 375];
 
 var studentRecords = [
-	{ id: 313, name: "Frank", paid: true, },
-	{ id: 410, name: "Suzy", paid: true, },
-	{ id: 709, name: "Brian", paid: false, },
-	{ id: 105, name: "Henry", paid: false, },
-	{ id: 502, name: "Mary", paid: true, },
-	{ id: 664, name: "Bob", paid: false, },
-	{ id: 250, name: "Peter", paid: true, },
-	{ id: 375, name: "Sarah", paid: true, },
-	{ id: 867, name: "Greg", paid: false, },
+  { id: 313, name: "Frank", paid: true },
+  { id: 410, name: "Suzy", paid: true },
+  { id: 709, name: "Brian", paid: false },
+  { id: 105, name: "Henry", paid: false },
+  { id: 502, name: "Mary", paid: true },
+  { id: 664, name: "Bob", paid: false },
+  { id: 250, name: "Peter", paid: true },
+  { id: 375, name: "Sarah", paid: true },
+  { id: 867, name: "Greg", paid: false },
 ];
 
 printRecords(currentEnrollment);
@@ -65,7 +21,7 @@ printRecords(currentEnrollment);
 console.log("----");
 remindUnpaid(currentEnrollment);
 
-/*
+/* Expected  Output
 	Bob (664): Not Paid
 	Henry (105): Not Paid
 	Sarah (375): Paid
@@ -82,3 +38,54 @@ remindUnpaid(currentEnrollment);
 	Bob (664): Not Paid
 	Henry (105): Not Paid
 */
+
+function getStudentFromId(studentId) {
+  return studentRecords.find(matchId);
+  // Reads cleaner and matchId is in the blue bucket
+  function matchId(record) {
+    return record.id == studentId;
+  }
+}
+
+function printRecords(recordIds) {
+  var records = recordIds.map(getStudentFromId);
+  records.sort(sortByNameAsc);
+  records.forEach(printRecord);
+}
+
+// Standalone function pulled out
+function sortByNameAsc(record1, record2) {
+  if (record1.name < record2.name) return -1;
+  else if (record1.name > record2.name) return 1;
+  else return 0;
+}
+
+function printRecord(record) {
+  console.log(
+    `${record.name} (${record.id}): ${record.paid ? "Paid" : "Not Paid"}`
+  );
+}
+
+function paidStudentsToEnroll() {
+  var recordsToEnroll = studentRecords.filter(needToEnroll);
+  var idsToEnroll = recordsToEnroll.map(getStudentId);
+  return [...currentEnrollment, ...idsToEnroll];
+}
+
+function needToEnroll(record) {
+  return record.paid && !currentEnrollment.includes(record.id);
+}
+
+function getStudentId(record) {
+  return record.id;
+}
+
+function remindUnpaid(recordIds) {
+  var unpaidIds = recordIds.filter(notYetPaid);
+  printRecords(unpaidIds);
+}
+
+function notYetPaid(studentId) {
+  var record = getStudentFromId(studentId);
+  return !record.paid;
+}
