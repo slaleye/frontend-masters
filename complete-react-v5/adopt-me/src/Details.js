ceadd495c8232904f38a7d2f5fabd8e647d85174
Salomon Laleye <salomonlaleye@gmail.com>
2020-06-24 21:14:49 +0200

    Add Async useEffect, add Reach Router, debugging

---
+++ complete-react-v5/adopt-me/src/Details.js
@@ -0,0 +1,9 @@
+import React from  'react';
+
+function Details(props){
+
+// return <pre><code>{JSON.stringify(props, null, 4)}</code></pre>;
+    return <h3>Hey</h3>;
+}
+
+export default Details;
